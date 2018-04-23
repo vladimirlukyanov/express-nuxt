29ff0fd83b1e4c417f843acb9d015260a9994035
Fred Salaysay <fsalaysay@gmail.com>
2018-04-23 13:34:28 +0800

    Init Commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <p>This is the Index Page</p>
+  </section>
+</template>
+
+<script>
+
+export default {
+  head () {
+    return {
+      title: 'Users'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
